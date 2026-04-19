c06851c755f6be3bbd8b49b0687106fc48523fb8
lopes37451 <37451@raphaeldisanto.com.br>
2026-04-19 17:57:39 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
     <div className="main">
+      <div className="left"></div>
 
-      {/* Lado esquerdo (imagem estilo Instagram) */}
-      <div className="left">
-        
-      </div>
-
-      {/* Lado direito (login) */}
       <div className="right">
         <div className="container">
-
           <div className="logo">Instagram</div>
+          <p className="subtitle">Entre para continuar</p>
 
-          <input type="text" placeholder="Telefone, nome de usuário ou email" />
-          <input type="password" placeholder="Senha" />
-
-          <button>Entrar</button>
+          <form className="form">
+            <input type="text" placeholder="Telefone, nome de usuário ou email" />
+            <input type="password" placeholder="Senha" />
+            <button type="submit">Entrar</button>
+          </form>
 
           <div className="divider">OU</div>
-
           <div className="forgot">Esqueceu a senha?</div>
-
         </div>
       </div>
-
     </div>
-  )
+  );
 }
